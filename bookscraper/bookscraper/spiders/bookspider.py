ac4ccc8936c6cef927eb7fa4a6480512fb62f3ea
avd <aravind19112003@gmail.com>
2023-08-20 17:14:43 +0530

    Adding data feed

--- bookscraper/bookscraper/spiders/bookspider.py
+++ bookscraper/bookscraper/spiders/bookspider.py
@@ -7,6 +7,10 @@ class BookspiderSpider(scrapy.Spider):
     allowed_domains = ['books.toscrape.com']
     start_urls = ['https://books.toscrape.com/']
 
+    custom_settings = {
+        'data.csv': {'format': 'csv', 'overwrite': True}
+    }
+
     def parse(self, response):
         books = response.css('article.product_pod')
         for book in books:
